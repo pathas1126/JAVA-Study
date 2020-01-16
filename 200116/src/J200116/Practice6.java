b870618449ad1b5e443054cd15230cabb79ed86b
pathas <pathas@naver.com>
2020-01-16 23:20:05 +0900

    200116 정리

--- 200116/src/J200116/Practice6.java
+++ 200116/src/J200116/Practice6.java
@@ -1,17 +1,16 @@
 package J200116;
 
 public class Practice6 {
-
 	public static void main(String[] args) {
+		// 콘솔창에 피라미드 만들기
 		for (int i = 1; i < 5; i++) {
-
-			for (int j = 1; j <= (i * 2) - 1; j++) {
-				System.out.print('*');
+			for (int k = 4; i<k; k--) {
+				System.out.print("  ");
 			}
-
-			System.out.println();
+			for (int j = 1; j <= (i * 2) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();		
 		}
-
 	}
-
 }
